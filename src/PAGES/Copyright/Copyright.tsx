bf1ef99f967ea493dbeeacd685cbbd1a3ba902c0
Karim Saipi <karim_saifu@mail.ru>
2025-11-19 21:45:09 +0300

    Normalize folder names to lowercase

--- src/PAGES/Copyright/Copyright.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Holders from '../../components/HoldersText/Holders'
-
-export default function Copyright() {
-  return (
-    <Holders/>
-  )
-}
--- src/PAGES/PublicationCreate/PublicationCreateForm.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import PublicationCreateForm from '../../components/PublicationCreateForm/PublicationCreateForm'
-
-export default function PublicationCreate() {
-  return (
-    <PublicationCreateForm/>
-  )
-}
